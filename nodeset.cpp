864d6999eb541b27278e4a5ec1e2d31c58a2fe41
christos2 <c.stratopoulos@gmail.com>
2015-01-16 00:37:11 -0500

    Create nodeset.cpp

---
+++ nodeset.cpp
@@ -0,0 +1,27 @@
+#include "nodeset.h"
+
+void makeset(node* x){
+  x->parent = x; x->rank = 0;
+}
+
+node* find(node* x){
+  node* p;
+  for (p = x; p->parent != p; p = p->parent)
+    ;
+  return p;
+}
+
+void swap(node*& x, node*& y){
+  node* temp = x;
+  x = y;
+  y = temp;
+}
+
+node* link(node* x, node* y){
+  if (x->rank > y->rank)
+    swap(x, y);
+  else if (x->rank == y->rank)
+    y->rank += 1;
+  x->parent = y;
+  return y;
+}
