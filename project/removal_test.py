279fb2fdd049fbaa251f358ea61987cf34ae5c2a
zero-impact <lawsonfulton@gmail.com>
2015-03-31 19:39:45 -0400

    removal test code working

--- project/removal_test.py
+++ project/removal_test.py
@@ -4,13 +4,34 @@ import ntpath
 
 def main():
 	problem_file = sys.argv[1]
+	node_count = 0
+
+	useless_fname = run_solvers(problem_file)
+	
+	solution_edges, node_count, node_count = load_edges("out.sol")
+	useless_edges, node_count, useless_edge_count = load_edges(useless_fname)
+	total_edge_count = node_count * node_count
+
+	print
+	conflict = check_for_conflicts(solution_edges, useless_edges)
+
+	if not conflict:
+		print "Test passed! No Conflicts found."
+	else:
+		print "Try again!"
+
+	print
+	print "Removed a total of %d edges out of %d. (%%%f)" % (useless_edge_count, total_edge_count, useless_edge_count/float(total_edge_count) * 100)
+	print "There are %d edges remaining." % (total_edge_count - useless_edge_count, )
+
+def run_solvers(problem_file):
 	prob_name = ntpath.basename(problem_file)
 
 	subprocess.call(["./edge_deletion", problem_file])
 
+	useless_fname = prob_name + "-useless.edg"
 	pruned_fname = prob_name + "-pruned.edg"
 	orig_fname = prob_name + "-orig.edg"
-	useless_fname = prob_name + "-useless.edg"
 
 	subprocess.call(["./test/prob2tsp", pruned_fname])
 	subprocess.call(["./test/concorde", "out.tsp"])
@@ -18,10 +39,7 @@ def main():
 	subprocess.call(["./test/prob2tsp", orig_fname])
 	subprocess.call(["./test/concorde", "-f", "out.tsp"]) #-f save as edge file "out.sol"
 
-	solution_edges = load_edges("out.sol")
-	useless_edges = load_edges(useless_fname)
-
-	print useless_edges
+	return useless_fname
 
 def load_edges(fname):
 	edges = {}
@@ -39,7 +57,23 @@ def load_edges(fname):
 			key = "%s,%s" % (e0, e1)
 			edges[key] = True
 
-	return edges
+	return edges, int(node_count), int(edge_count)
+
+def check_for_conflicts(solution_edges, useless_edges):
+	found_conflict = False
+
+	for edge in useless_edges:
+		if edge in solution_edges:
+			print "You messed up!"
+			print "Removed edge %s that was found in the optimal tour!" % edge
+
+			found_conflict = True
+
+	return found_conflict
 
 if __name__ == "__main__":
 	main()
+
+
+
+
