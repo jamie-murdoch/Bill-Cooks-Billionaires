b4eae76b2c3d3c08cba8a7202ba1231c87eb4aab
Jamie Murdoch <jamie.murdoch11@gmail.com>
2015-03-31 23:04:59 +0100

    Fixed step 2

--- project/edge_deletion.cpp
+++ project/edge_deletion.cpp
@@ -261,7 +261,7 @@ static int delete_edges(Graph &g)
 
 
 static int delete_edges2(Graph &g){
-  const int num_points = 50;
+  const int num_points = 10;
 
 #pragma omp parallel for schedule(dynamic)
   for(int h = 0; h < g.edges.size(); h++){
@@ -288,7 +288,7 @@ static int delete_edges2(Graph &g){
 
       vector<int> compatible_points;
       for(int x = 0; x < g.node_count(); x++)
-	if(x != p && x != q && is_edge(r, x, g) && are_compatible(p, q, r, x, g)) compatible_points.push_back(x);
+	if(is_edge(r, x, g) && are_compatible(p, q, r, x, g)) compatible_points.push_back(x);
       
       for(vector<int>::iterator x = compatible_points.begin(); x != compatible_points.end(); ++x){
 	for(vector<int>::iterator y = x; y != compatible_points.end(); ++y){
